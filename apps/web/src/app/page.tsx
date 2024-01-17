2018cb1d92b7a24d9c5d8515b6347a717ee0c13c
Bridger Tower <bridgertower@gmail.com>
2024-01-17 10:20:49 -0700

    fix: craftui link

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,24 @@
+// @ts-expect-error bc turbo
 import { Button } from "@repo/ui/ui/button";
+// @ts-expect-error bc turbo
 import { Separator } from "@repo/ui/ui/separator";
+// @ts-expect-error bc turbo
+import * as Craft from "@repo/ui/craft/layout";
+import { Main } from "@repo/ui/craft/layout";
 
 export default function Page(): JSX.Element {
   return (
-    <main className="p-12 flex flex-col gap-8">
+    <Main className="p-12 flex flex-col gap-8">
       <h1 className="text-5xl antialiased text-primary">Hello World!</h1>
       <Separator />
-      <Button className="w-fit" variant={"secondary"}>
-        Test
-      </Button>
-    </main>
+      <Craft.Section>
+        <Craft.Container>
+          <h2>Here is a Button from shadcn/ui</h2>
+          <Button className="w-fit" variant={"primary"}>
+            Test
+          </Button>
+        </Craft.Container>
+      </Craft.Section>
+    </Main>
   );
 }
